a021783cf94148c58fa55df3ae5db3120c550c9a
reivax-boucoi <reivax.boucoi@laposte.net>
2020-05-27 10:39:39 +0200

    a

--- src/sketch_ino.cpp
+++ src/sketch_ino.cpp
@@ -39,13 +39,8 @@ Screen 2
 
 Text           t1s2("Heater:",0,0);
 Text           t2s2("Cooling:",0,1);
-<<<<<<< HEAD
-EditableText   t3s2(setHeater,11,0,3);
-EditableText   t4s2(setCool,11,1,3);
-=======
 EditableText   t3s2("ON",11,0,3);
 EditableText   t4s2("DIS",11,1,3);
->>>>>>> 0ebb94e4daee49682149e5cded7b2e8c20d2b780
 Screen         scr2(&t1s2,"Heating");
 
 /*
@@ -59,11 +54,7 @@ Screen 3
 Text           t1s3("Luminosity:",0,0);
 Text           t2s3("Light:",0,1);
 EditableValue  v1s3(&setLum,13,0,3,0,900);
-<<<<<<< HEAD
-EditableText   t3s3(setLight,9,1,3);
-=======
 EditableText   t3s3("ON",9,1,3);
->>>>>>> 0ebb94e4daee49682149e5cded7b2e8c20d2b780
 Screen         scr3(&t1s3,"Luminosity");
 
 /*
